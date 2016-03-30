e583f6e164b6cdc8d9c883549a1a741b719b2cf0
MayankGupta1 <mayank1.gupta@paytm.com>
2016-03-30 19:11:13 +0530

    Update PaytmConstant

--- ChecksumVB.Net/App_Code/PaytmContant.vb
+++ ChecksumVB.Net/App_Code/PaytmContant.vb
@@ -4,11 +4,6 @@ Namespace PaytmContant
     Public NotInheritable Class PaytmConstants
         Public Shared MID As String = "xxxxxxxxxxxxx"
         Public Shared MERCHANT_KEY As String = "xxxxxxxxx"
-        Public Shared INDUSTRY_TYPE_ID As String = ""
-        Public Shared CHANNEL_ID As String = ""
-        Public Shared WEBSITE As String = ""
         
     End Class
 End Namespace
-
-
